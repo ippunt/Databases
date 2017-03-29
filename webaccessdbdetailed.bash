427dcf57cd0051766795087993677605846c0c8c
Jeffrey <jeffrey.darling15@gmail.com>
2017-03-28 22:28:18 -0700

    Add files via upload
    gather detailed information from all the ips that have visited web site.

---
+++ webaccessdbdetailed.bash
@@ -0,0 +1,40 @@
+#!/bin/bash
+ 
+sqlite3 ipvisitors.sqlite << HEREDOC
+
+create table if not exists "Visits"
+ (
+  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+  "ip" char(42) ,
+  "country_code" char(2) ,
+
+  "country_name" char(25) ,
+  "region_code" char(10) ,
+  "region_name" char(15) ,
+  "city" char(15) ,
+
+  "zip_code" char(10) ,
+  "time_zone" char(20) ,
+  "latitude" char(10) ,
+  "longitude" char(10) ,
+  "metro_code" char(5)
+);
+
+HEREDOC
+
+echo "ip,country_code,country_name,region_code,region_name
+,city,zip_code,time_zone,latitude,longitude,metro_code" > ~/output
+lst=$(cat ~/log2) #log file of all the ip addresses
+for x in $lst
+do
+curl freegeoip.net/csv/$x >> ~/output
+done
+cat ~/output | sed 's/,/|/g' >> ~/output1.sqlite
+echo ".import /home/jdarling2/output1.sqlite TMPVisits" | sqlite3 ipvisitors.sqlite
+
+echo "insert into Visits (ip, country_code, country_name, region_code, region_name,city, zip_code, time_zone,
+ latitude, longitude, metro_code) select ip, country_code, country_name, region_code, region_name, city, zip_code,time_zone,
+ latitude, longitude, metro_code from TMPVisits;" | sqlite3 ipvisitors.sqlite
+
+
+
